8da8f2225757ffd545a3993dfde1925acceb937a
siddshrestha <45787147+siddshrestha@users.noreply.github.com>
2021-03-02 17:14:27 +0545

    vuejs

---
+++ app.js
@@ -0,0 +1,31 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        name: 'Siddhartha',
+        job: 'Frontend Developer', 
+        website: 'https://www.linkedin.com/in/siddhartha-shrestha-b93277190/',
+        websiteTag: '<a href="https://www.linkedin.com/in/siddhartha-shrestha-b93277190/">Sidd Linkedin </a>',
+        age: 24,
+        x: 0,
+        y: 0
+    },
+    methods: {
+        greet: function(time){
+            
+            return 'Good' + time + ' ' + this.name;
+        },
+
+        add: function(inc){
+            this.age += inc;
+        },
+
+        sub: function(dec){
+            this.age -= dec;
+        },
+
+        updateXY: function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        }
+    },
+});
