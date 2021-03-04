79e0c38379b019db49525c8e6665ebbe63dcf3be
siddshrestha <45787147+siddshrestha@users.noreply.github.com>
2021-03-04 17:52:36 +0545

    punching bag game

--- day3/app.js
+++ day3/app.js
@@ -9,7 +9,27 @@ new Vue({
             { name: 'Rishab', age: 18},
         ],
         error: false,
-        success: false
+        success: false,
+
+        health: 100, 
+        ended: false
+    },
+
+    methods: {
+        punch: function(){
+            this.health -=10;
+            if(this.health <= 0){
+                this.ended = true;
+            }
+        },
+
+        restart: function(){
+            this.health = 100;
+            this.ended = false;
+        }
+
+
+    
     }
 
 })
