d9731389050aa254ae00359a1b6214c22a3c923a
siddshrestha <45787147+siddshrestha@users.noreply.github.com>
2021-03-04 13:58:25 +0545

    vfor

--- day3/app.js
+++ day3/app.js
@@ -2,8 +2,14 @@ new Vue({
     el: '#vue-app',
 
     data: {
+        characters: ['Siddhartha', 'Saurav', 'Rishab', 'Prajit'],
+        ninjas: [
+            { name: 'Sidd', age: 24},
+            { name: 'Saurav', age: 24},
+            { name: 'Rishab', age: 18},
+        ],
         error: false,
         success: false
     }
-    
+
 })
